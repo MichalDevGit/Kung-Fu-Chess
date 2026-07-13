97a4808ffcc04321f55cc142e7b5d9f962d96396
MichalDevGit <michali4419@gmail.com>
2026-07-13 10:00:45 +0300

    Engine: adding Game Engine

---
+++ Engine/GameEngine.cpp
@@ -0,0 +1,41 @@
+#include "GameEngine.h"
+
+GameEngine::GameEngine(const GameState& gameState)
+    : gameState(gameState)
+{
+}
+
+MoveValidation GameEngine::move(
+    const Position& from,
+    const Position& to)
+{
+    MoveValidation validation =
+        ruleEngine.validateMove(
+            gameState.getBoard(),
+            from,
+            to);
+
+    if (!validation.isValid)
+    {
+        return validation;
+    }
+
+    gameState.getBoard().movePiece(from, to);
+
+    return validation;
+}
+
+const GameState& GameEngine::getGameState() const
+{
+    return gameState;
+}
+
+Board& GameState::getBoard()
+{
+    return board;
+}
+
+const Board& GameState::getBoard() const
+{
+    return board;
+}
---
+++ Engine/GameEngine.h
@@ -0,0 +1,26 @@
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
+
+#include "../Model/GameState.h"
+#include "../Rules/RuleEngine.h"
+#include "../Common/MoveValidation.h"
+
+class GameEngine
+{
+private:
+    GameState gameState;
+    RuleEngine ruleEngine;
+    Board& getBoard();
+    const Board& getBoard() const;
+
+public:
+    GameEngine(const GameState& gameState);
+
+    MoveValidation move(
+        const Position& from,
+        const Position& to);
+
+    const GameState& getGameState() const;
+};
+
+#endif
