462df9217abbaff8088ba21f30fb8dd07b1b7c4f
MichalDevGit <michali4419@gmail.com>
2026-07-13 09:49:02 +0300

    rules: adding rule engine

--- rules/RuleEngine.cpp
+++ rules/RuleEngine.cpp
@@ -1,45 +1,26 @@
-#include "RuleEngine.h"
-
-MoveValidation RuleEngine::validateMove(
-    const Board& board,
-    const Position& from,
-    const Position& to) const
+switch (piece->getType())
 {
-    // מקור בתוך הלוח
-    if (!board.isValidPosition(from))
+    case PieceType::Rook:
     {
-        return { false, MoveValidationReason::SourceOutsideBoard };
+        auto legalMoves =
+            rookRule.legalDestinations(board, *piece);
+
+        if (legalMoves.contains(to))
+        {
+            return MoveValidation(
+                true,
+                MoveValidationReason::None);
+        }
+
+        break;
     }
 
-    // יעד בתוך הלוח
-    if (!board.isValidPosition(to))
-    {
-        return { false, MoveValidationReason::DestinationOutsideBoard };
-    }
-
-    // יש כלי במקור?
-    const Piece* piece = board.getPiece(from);
-
-    if (piece == nullptr)
-    {
-        return { false, MoveValidationReason::NoPieceAtSource };
-    }
-
-    // יש כלי מאותו צבע ביעד?
-    const Piece* destination = board.getPiece(to);
-
-    if (destination != nullptr &&
-        destination->getColor() == piece->getColor())
-    {
-        return { false, MoveValidationReason::FriendlyPieceAtDestination };
-    }
-
-    switch (piece->getType())
-    {
-        case PieceType::Rook:
-            return rookRule.isLegalMove(board, *piece, to);
-
-        default:
-            return { false, MoveValidationReason::UnsupportedPiece };
-    }
+    default:
+        return MoveValidation(
+            false,
+            MoveValidationReason::UnsupportedPiece);
 }
+
+return MoveValidation(
+    false,
+    MoveValidationReason::IllegalMove);
--- rules/RuleEngine.h
+++ rules/RuleEngine.h
@@ -1,21 +1,11 @@
-#ifndef RULEENGINE_H
-#define RULEENGINE_H
-
-#include "../Common/MoveValidation.h"
-#include "../Model/Board.h"
-
-#include "RookRule.h"
-
 class RuleEngine
 {
-private:
-    RookRule rookRule;
-
 public:
     MoveValidation validateMove(
         const Board& board,
         const Position& from,
         const Position& to) const;
-};
 
-#endif
+private:
+    RookRule rookRule;
+};
